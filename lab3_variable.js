94fb5789a19f310435b7e697bed881380b399e26
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-30 15:38:47 +0700

    DataType: Number/ String/ Boolean

--- lab3_variable.js
+++ lab3_variable.js
@@ -4,7 +4,8 @@
 // a : ten bien
 // value : gia tri
 
-
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến sum, sub, mul, div lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
 
 const log = console.log;
 
---
+++ lab4_dataType.js
@@ -0,0 +1,22 @@
+/**
+ * Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
+ * Ví dụ dưới đây mô tả thời tiết ngày hôm nay
+ *  var today = 'Sunday';
+ *  var isCloudy = true;
+ *  var currentTemperature = 30;
+ *
+ * fullName có kiểu dữ liệu là string;
+ * age có kiểu dữ liệu là number;
+ * isMale có kiểu dữ liệu là boolean;
+ */
+
+// viết code ở đây
+const log = console.log;
+var fullName;
+var age;
+var isMale;
+
+fullName = 'soucre3thaipvt';
+age = 25;
+isMale = true;
+log(fullName + " " + age + " " + isMale);
