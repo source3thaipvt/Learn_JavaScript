b4e7f319582e7dceb4c2364a5dbd99ade25b750f
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-30 17:35:47 +0700

    Cach tao 1 function, goi 1 function

---
+++ lab10_function.js
@@ -0,0 +1,35 @@
+// may pha cafe la 1 function(hàm) có đầu vào đầu ra.
+
+//      Define a function: thuc thi(Xac dinh) 1 function
+
+// function doSomeThing(input1,input2,...){
+//     //code
+//     return something<ra thu gi do>;
+// }
+// Keywords: function, return;
+// functionName: doSomeThing<lam gi do>
+// Paramenter name: input1<cafe>, input2<nuoc>, .....
+
+//      Execute/Call a function: Thuc thi/ goi 1 function
+// doSomeThing(input1,input2,...)
+// đầu vào khác nhau, input1,input2;
+
+// khai bao function
+function tinhDienTichTamGiac(a, h) {
+    var s = a * h / 2;
+    return console.log(s);
+}
+
+//goi function
+tinhDienTichTamGiac(20, 8);
+
+
+//Tinh dien tich hinh tron : S = r * r * 3.14
+
+function tinhDienTichHinhTron(r) {
+    var s = r * r * 3.14;
+    return console.log(s);
+}
+
+tinhDienTichHinhTron(10);
+
