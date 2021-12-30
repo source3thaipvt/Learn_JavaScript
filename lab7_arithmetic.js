b03f49ec5d3b06617b08dfd08b5e0dbe407c3596
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-30 16:44:43 +0700

    Arithmectic:Toan Tu: + - * / %

---
+++ lab7_arithmetic.js
@@ -0,0 +1,35 @@
+// Arithmetic Operators: Toán tử trong js
+// 1. ++  --
+// 2. *   /   %
+// 3. +   -
+
+// chia lay du, lay ket qua du
+var a = 9 % 4;
+console.log(a);
+
+5 - 12 + 7;
+// (5-12) + 7
+// 0
+// thực hiện từ trái qua phải
+2 * 24 % 5;
+// (2*24) % 5
+// 48 % 5 = dư 3
+
+4 - 6 / 3 % 4;
+// 4 - (6/3) % 4
+// 4 - (2 % 4) 
+// 4 - 2
+// 2 
+// nhân chia trước cộng trừ sau
+
+
+/**
+ * a: cạnh đáy của 1 hình tam giác
+ * h: chiều cao của tam giác đó
+ * Yêu cầu: khai báo biến s bằng diện tích của hình tam giác này 
+ */
+ 
+// viết code ở đây.
+var a = 10;
+var h = 20;
+var s = (a*h)/2;
