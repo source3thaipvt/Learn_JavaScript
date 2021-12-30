9fcf05677f37edb3b442ec87a2c48e8640c9a763
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-30 15:28:30 +0700

    Cach Khai bao bien Variable

---
+++ lab3_variable.js
@@ -0,0 +1,22 @@
+//khai bao bien
+// var a = value;
+// var : keywork
+// a : ten bien
+// value : gia tri
+
+
+
+const log = console.log;
+
+var a = 7;
+var b = 9;
+var sum = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+
+log(sum);
+log(sub);
+log(mul);
+log(div);
+
