f22d71fd76d44775f7e830fd95447e614ad8b245
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-30 17:12:02 +0700

    Arithmectic:Toan Tu Gan: = -= += /= *= %=

---
+++ lab9_assignment.js
@@ -0,0 +1,32 @@
+// Arithmetic Operators: Toán tử trong js
+// 1. ++  --
+// 2. *   /   %
+// 3. +   -
+// 4. Phep gan: = += -= *= /= %=
+
+var a;
+var b;
+// gán nhiều biến vào 1 giá trị
+a = b = 1;
+// a = b; b = 1;
+
+a += 5;
+// a = a + 5
+
+a -= 4;
+// a = a - 4
+
+a *= 2;
+// a = a * 2
+
+a /= 5;
+// a = a / 5
+
+a %= 3;
+// a = a % 3
+
+a *= b / 2;
+// a = a * b / 2
+
+
+
