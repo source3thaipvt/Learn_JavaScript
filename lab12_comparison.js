32d8658fdcffd2884ef6a4db57a006d0ab52b274
soucre.thaipvt <soucre.thaipvt@gmail.com>
2021-12-31 10:35:52 +0700

    Comparison:Toan tu so sanh: > >= < <= === !==

---
+++ lab12_comparison.js
@@ -0,0 +1,11 @@
+// Bài 7: Arithmetic operators: toán tử + - * / %
+// Bài 8: Increment / Decrement: Phép tính -- ++
+// Bài 9: Assignment operators:  Toán tử gán: += -= *= /+ %= =
+// Bài 12: Comparison operators: 
+// toán tử so sánh > >= < <= === !== (Bài khác: == !=)
+
+5 > 5; // tra ve false
+5 >= 5; // tra ve true
+-1<=-2; // tra ve false
+10 === 100; // tra ve false //so sanh bang
+10 !== 10; // tra ve false  //so sanh khac
